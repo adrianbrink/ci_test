9bd95477036501a93a14841cadb70532e60937fb
adrianbrink <adrian@brink-holdings.com>
2017-01-04 16:49:18 +0100

    add redis

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
+extern crate redis;
+use redis::Commands;
+
 pub fn add_two(x: i32) -> i32 {
     x + 2
+}
+
+pub fn fetch_an_integer() -> redis::RedisResult<isize> {
+    // connect to redis
+    let client = try!(redis::Client::open("redis://127.0.0.1/"));
+    let con = try!(client.get_connection());
+    // throw away the result, just make sure it does not fail
+    let _: () = try!(con.set("my_key", 42));
+    // read back the key and return it.  Because the return value
+    // from the function is a result for integer this will automatically
+    // convert into one.
+    con.get("my_key")
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate ci_test;
+extern crate redis;
 
-use ci_test::add_two;
+use ci_test::{add_two, fetch_an_integer};
 
 fn main() {
     println!("{:?}", add_two(2));
+    let result = fetch_an_integer().unwrap();
+    println!("{:?}", result);
 }
